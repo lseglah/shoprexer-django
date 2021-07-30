04c966391e550a1415497f32a0235a811a31b4ba
lseglah <loopyspot@gmail.com>
2021-07-30 16:49:49 +0000

    handled cart and checkout page for the logged in users

--- shoprexer/settings.py
+++ shoprexer/settings.py
@@ -148,7 +148,7 @@ MESSAGE_TAGS = {
 
 # SMTP configration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_HOST_USER = 'loopyspot@gmail.com'
 EMAIL_HOST_PASSWORD = 'Craze360'
 EMAIL_USE_TLS = True
